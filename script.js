e5dd0eed383513ed68297e642acb09935751168f
dhanush <dhanushpvenkitesh@gmail.com>
2024-07-12 11:14:30 +0530

    second

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ console.log('leftTrimmed String length : ',leftTrimmedValue.length);
 let rightTrimmedValue = str.trimEnd();
 console.log("rightTrimmedValue :",rightTrimmedValue);
 console.log('rightTrimmed String length :', leftTrimmedValue.length);
+
+
+
+
+
+// array
+
+
+
+
+// objects
+
+let obj ={
+    firstname : "anu",
+    lastname : "nelson",
+    age  : 10,
+    address: {
+        city: 'my city',
+        street : 'my street',
+    },
+    canadian: true,
+    hobbies : ['gamming','reading','books'],
+
+}
+console.log('obj :',obj)
+obj.mark1=90;
+console.log("obj : ",obj);
+
+
+
+let json_obj = JSON.stringify(obj);
+console.log("json_obj :", josn);
+console.log("typeof obj : ",typeof obj);
+console.log("firstname :", json_obj.firstname);
+
+
+
+let parsed_json_obj =JSON.parse(json_obj);
+console.log("parsed_json_obj :" ,parsed_json_obj)
